7f3313b610514b135bbe8dda84fb797be728c37b
adm1255 <96832058+adm1255@users.noreply.github.com>
2022-12-15 22:13:19 +0600

    JS

---
+++ k.js
@@ -0,0 +1,16 @@
+ prompt ('Введите имя')
+ 
+ prompt ('Введите фамилию')
+
+ prompt ('Введите возраст')
+
+ 
+ var firstName = prompt ('Введите имя')
+ var lastName =  prompt ('Введите фамилию')
+ var age = prompt ('Введите возраст')
+
+ console.log("Пользователь: "+ " " +firstName + " " + lastName );
+
+ console.log('Возраст:' + "" + age);
+
+
